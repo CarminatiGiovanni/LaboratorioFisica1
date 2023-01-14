a899b8be546cc898d25939647c9b9042f70be92d
HeyJOe <jocarmi03@gmail.com>
2023-01-14 16:46:29 +0100

    before branch

--- VISCOSITA/grafici.py
+++ VISCOSITA/grafici.py
@@ -26,18 +26,28 @@ for item in counter:
     valore.append(item)
     ripetizioni.append(counter[item])
 
+
+# istogramma
 x_indexes = np.arange(len(valore))
-
 plt.bar(x_indexes,ripetizioni) # disegna l'istogramma
-
-plt.title("r = 3mm d = 10cm N = 100 ") # aggiunge il titolo
-
 plt.yticks(ticks=np.arange(0,max(counter.values())+2,2))
 plt.xticks(ticks=x_indexes,labels=valore, rotation=45) # aggiunge titoli asse x
 
+#gaussiana
+
+n = len(valore) * 3
+x_val = np.linspace(valore[0],valore[len(valore)-1])
+print(x_val)
+x = np.arange(n)
+#y = np.arange(len(x_val))
+y_exp = np.array((1/(np.sqrt(2*np.pi)*sigma)) * np.exp(-(np.power(x_val - media,2)/(2*sigma*sigma))))
+#print(y_exp)
+
+plt.plot(x_val,y_exp, color = "#ff0000")
+
+plt.title("r = 3mm d = 10cm N = 100 ") # aggiunge il titolo
 plt.xlabel("Misurazione")
 plt.ylabel("n ripetizioni")
-
 plt.tight_layout()
 
 plt.show()
