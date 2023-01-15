37165b4d4b75e8bb0dd66e4b27e92b52bbfc3968
HeyJOe <jocarmi03@gmail.com>
2023-01-15 18:06:47 +0100

    added chisquare

--- VISCOSITA/viscosita.py
+++ VISCOSITA/viscosita.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+import scipy.stats as sc
 
 def fdp(x,m,s):
     h = 1./s/np.sqrt(2)
@@ -23,7 +24,7 @@ m = round(media,2) #voglio che la media sia al centro di un intervallo
 i = round(sigma/2,2) #voglio che ogni intervallo sia largo sigma/2
 offset = round(i/2,3)
 bins = np.concatenate((np.flip(np.arange(m - offset - 0.005,min_ - i,-i)),np.arange(m+offset - 0.005,max_,i)))
-print(bins)
+
 plt.hist(data,bins=bins, density=True, label= 'data', color="#89c4ff", edgecolor='black')
 plt.xticks(bins, rotation=45)
 
@@ -36,5 +37,7 @@ plt.xlabel("$t_{caduta}$ (s)")
 
 plt.legend()
 plt.title("Misurazione sfera in caduta nella glicerina\n$\\bf{t_{caduta} = " + str(VALORE) + " s}$\n")
+plt.tight_layout()
 plt.show()
 
+print(sc.chisquare(data))
