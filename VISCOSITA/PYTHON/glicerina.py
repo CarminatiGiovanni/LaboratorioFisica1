43cd3f64bca0160a4f0a71a0f8a2fea8a6af2fc4
HeyJOe <jocarmi03@gmail.com>
2023-02-02 10:09:52 +0100

    misurazione strana

--- VISCOSITA/PYTHON/glicerina.py
+++ VISCOSITA/PYTHON/glicerina.py
@@ -1,12 +1,23 @@
 import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
+from typing import Final as const
 
-massa_5sferette_5mm = 0.0025494 #kg
-massa_sferetta_5mm = massa_5sferette_5mm / 5
-volume_sferetta_5mm = (4.0/3)*np.pi*np.power(0.005,3)
-densita_sferetta = round(massa_sferetta_5mm / volume_sferetta_5mm,1)
+raggio_sferetta: const = 0.005 / 2
+massa_5sferette_5mm: const = 0.0025494 #kg
+massa_sferetta_5mm: const = massa_5sferette_5mm / 5
+volume_sferetta_5mm: const = (4.0/3)*np.pi*np.power(raggio_sferetta,3) # 
+densita_sferetta: const = round(massa_sferetta_5mm / volume_sferetta_5mm,0)
 
-print(densita_sferetta)
+densita_glicerina: const = 1249 #kg/m3
+temperatura: const = 24.5 # °C
+
+v_limite_misurata = 0.1/2.985 # spazio/tempo misurato in misure ed errori
+
+viscosita = 2 * 9.81 * (raggio_sferetta**2) * (densita_sferetta - densita_glicerina) / (9 * v_limite_misurata)
+
+print(f"viscosità misurata: {viscosita}")
+aspettazione = np.exp(-2.18-0.099*temperatura)
+
+print(f"viscosità aspettata: {aspettazione}")
+
 
 # 1.256
