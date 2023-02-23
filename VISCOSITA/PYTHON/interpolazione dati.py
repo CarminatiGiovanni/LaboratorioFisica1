8f8375281d5bf36ca8d141a528ad32f9de796a7e
HeyJOe <jocarmi03@gmail.com>
2023-02-23 16:00:30 +0100

    fixes and in glicerina ricordarsi di misurare il peso delle sferette da 3mm che abbiamo più misure

--- VISCOSITA/PYTHON/interpolazione dati.py
+++ VISCOSITA/PYTHON/interpolazione dati.py
@@ -1,51 +1,50 @@
 import numpy as np
-from mpl_toolkits.axisartist.axislines import AxesZero
 import pandas as pd
 from matplotlib import pyplot as plt
 
 import os
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 FILE = dir_path + '/../CSV/' + 'T=24.5°C.csv'
 
-
-fr = pd.read_csv(FILE) #fileread
+fr = pd.read_csv(FILE)  # fileread
 mm2 = np.array(fr["2"])
 mm3 = np.array(fr["3"])
 mm4 = np.array(fr["4"])
 mm5 = np.array(fr["5"])
 mm6 = np.array(fr["6"])
 
-data = np.array([mm2,mm3,mm4,mm5,mm6])
+data = np.array([mm2, mm3, mm4, mm5, mm6])
 
-t_medie = np.array(list(np.mean(i) for i in data)) # calcola le medie dei tempi per ogni diametro
-v_medie = np.concatenate(([0],0.2/t_medie)) # calcola le medie delle velocità per ogni diametro (primo valore 0)
+t_medie = np.array(list(np.mean(i) for i in data))  # calcola le medie dei tempi per ogni diametro
+v_medie = np.concatenate(([0], 0.2 / t_medie))  # calcola le medie delle velocità per ogni diametro (primo valore 0)
 
-#k = (plt.style.available)
-#plt.style.use(stile)
+#  k = (plt.style.available)
+#  plt.style.use(stile)
 
-plt.figure("studio misure sfere di raggio variabile",figsize=(15,8)) # set titolo grafici
+plt.figure("studio misure sfere di raggio variabile", figsize=(15, 8))  # set titolo grafici
 
 # costruisce gli spazi in cui dividere i grafici
 plt_retta = plt.subplot2grid((3, 3), (0, 0))
 plt_parabola = plt.subplot2grid((3, 3), (1, 0))
 plt_log = plt.subplot2grid((3, 3), (2, 0))
-plt_accordo = plt.subplot2grid((3, 3), (0, 1), colspan=2,rowspan=3)
+plt_accordo = plt.subplot2grid((3, 3), (0, 1), colspan=2, rowspan=3)
 
-plt_retta.plot([0,2,3,4,5,6],v_medie, color="red")
+plt_retta.plot([0, 2, 3, 4, 5, 6], v_medie, color="red")
 plt_retta.set_title("raggio-velocità")
-plt_retta.set_ylim(bottom = 0)
+plt_retta.set_ylim(bottom=0)
 plt_retta.set_xlim(left=0)
 
-plt_parabola.plot([0,4,9,16,25,36],v_medie, color="orange")
+plt_parabola.plot([0, 4, 9, 16, 25, 36], v_medie, color="orange")
 plt_parabola.set_title("$raggio^{2}$-velocità")
-plt_parabola.set_ylim(bottom = 0)
+plt_parabola.set_ylim(bottom=0)
 plt_parabola.set_xlim(left=0)
 
-plt_log.plot([0,2,3,4,5,6],v_medie, color="purple")
+plt_log.plot([0, 2, 3, 4, 5, 6], v_medie, color="purple")
 plt_log.set_xscale('log')
 plt_log.set_yscale('log')
 plt_log.set_title("raggio-velocità (log)")
-plt_log.set_ylim(bottom = 0)
+plt_log.set_ylim(bottom=0)
 plt_log.set_xlim(left=0)
 
 # interpoliamo // la ringraziamo
@@ -53,40 +52,40 @@ plt_log.set_xlim(left=0)
 # Costruiamo la retta che meglio descrive i nostri valori:
 # y = B + Ax
 
-y = v_medie[1:] # rinomino per comodità
-x = np.array([4,9,16,25,36], dtype=np.float16)
+y = v_medie[1:]  # rinomino per comodità
+x = np.array([4, 9, 16, 25, 36], dtype=np.float16)
 N = len(x)
 
-delta = N * np.sum(np.power(x,2)) - np.power(np.sum(x),2) # Δ
+delta = N * np.sum(np.power(x, 2)) - np.power(np.sum(x), 2)  # Δ
 
-A = (np.sum(np.power(x,2))*np.sum(y)-np.sum(x)*np.sum(x*y))/delta # A
-B = (N*np.sum(x*y)-np.sum(x)*np.sum(y))/delta #B
+A = (np.sum(np.power(x, 2)) * np.sum(y) - np.sum(x) * np.sum(x * y)) / delta  # A
+B = (N * np.sum(x * y) - np.sum(x) * np.sum(y)) / delta  # B
 
-v = y - A - B * x # v dovrebbe essere più piccola possibile
-sigmaY = np.sqrt((1.0/(N-2)) * np.sum(np.power(y - A - B * x,2))) # incertezza sulla Y (notare correzione bessel -2)
-sigmaA = sigmaY * np.sqrt(np.sum(x*x)/delta) # incertezza su A
-sigmaB = sigmaY * np.sqrt(N/delta) # incertezza su B
+v = y - A - B * x  # v dovrebbe essere più piccola possibile
+sigmaY = np.sqrt(
+    (1.0 / (N - 2)) * np.sum(np.power(y - A - B * x, 2)))  # incertezza sulla Y (notare correzione bessel -2)
+sigmaA = sigmaY * np.sqrt(np.sum(x * x) / delta)  # incertezza su A
+sigmaB = sigmaY * np.sqrt(N / delta)  # incertezza su B
 
 print(F"""
-    delta: {round(delta,4)}
-    A: {round(A,4)}
-    sigmaA: {round(sigmaA,4)}
+    delta: {round(delta, 4)}
+    A: {round(A, 4)}
+    sigmaA: {round(sigmaA, 4)}
 
-    B: {round(B,4)}
-    sigmaB: {round(sigmaB,4)}
+    B: {round(B, 4)}
+    sigmaB: {round(sigmaB, 4)}
 
-    sigmaY: {round(sigmaY,4)}
+    sigmaY: {round(sigmaY, 4)}
 
 """)
 
-
-x_exp = np.linspace(0,max(x) + 2,100)
+x_exp = np.linspace(0, max(x) + 2, 100)
 y_exp = A + B * x_exp
 
-plt_accordo.plot(x_exp,y_exp, color='blue')
-plt_accordo.errorbar(x,y,yerr=sigmaY,fmt='o',ecolor='black',color="#1ff24a",capsize=10)
+plt_accordo.plot(x_exp, y_exp, color='blue')
+plt_accordo.errorbar(x, y, yerr=sigmaY, fmt='o', ecolor='black', color="#1ff24a", capsize=10)
 plt_accordo.set_title('accordo punti-retta')
-plt_accordo.set_ylim(bottom = 0)
+plt_accordo.set_ylim(bottom=0)
 plt_accordo.set_xlim(left=0)
 
 # set the spacing between subplots
--- VISCOSITA/PYTHON/misure ed errori.py
+++ VISCOSITA/PYTHON/misure ed errori.py
@@ -4,39 +4,44 @@ from typing import Final as const
 from matplotlib import pyplot as plt
 import scipy.stats as sc
 import os
+
 Array = np.array
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 FILE = dir_path + '/../CSV/' + 'T=24.5°C 100 misure d=10cm.csv'
 
-fr = pd.read_csv(FILE)                          #fileread
-data = np.array(fr['misura r=3mm d = 10cm'])    #legge colonna 'misura r=3mm d = 10cm'
+fr = pd.read_csv(FILE)  # fileread
+data = np.array(fr['misura r=3mm d = 10cm'])  # legge colonna 'misura r=3mm d = 10cm'
 
-N:              const = len(data)               # numero di misure
-sigma:          const = np.std(data)            # sigma
-sigma_media:    const = sigma/np.sqrt(N)        # sigma tmedio
-min_,max_             = min(data),max(data)     # massimo e minimo valore
-media:          const = np.mean(data)           # t medio
-var:            const = sigma*sigma             # varianza (sigma quadro)
-CHIquadro:      const = sc.chisquare(data,ddof=2).pvalue       # test del chiquadro
+N: const = len(data)  # numero di misure
+sigma: const = np.std(data)  # sigma
+sigma_media: const = sigma / np.sqrt(N)  # sigma tmedio
+min_, max_ = min(data), max(data)  # massimo e minimo valore
+media: const = np.mean(data)  # t medio
+var: const = sigma * sigma  # varianza (sigma quadro)
+CHIquadro: const = sc.chisquare(data, ddof=2).value  # test del chiquadro
+print(type(CHIquadro))
 
-VALORE: str = "{0:.3f} ± {1:.3f}".format(media,sigma_media)
+VALORE: str = "{0:.3f} ± {1:.3f}".format(media, sigma_media)
 
-m = round(media,2)      # voglio che la media sia al centro di un intervallo
-i = round(sigma/2,2)    # voglio che ogni intervallo sia largo sigma/2
-offset = round(i/2,3)   # per avere i valori del tipo 0.005 per far si che ogni misura cada in un preciso intervallo
-bins = np.concatenate((np.flip(np.arange(m - offset - 0.005,min_ - i,-i)),np.arange(m+offset - 0.005,max_+i,i))) # crea una lista di valori centrata
+m = round(media, 2)  # voglio che la media sia al centro di un intervallo
+i = round(sigma / 2, 2)  # voglio che ogni intervallo sia largo sigma/2
+offset = round(i / 2, 3)  # per avere i valori del tipo 0.005 per far si che ogni misura cada in un preciso intervallo
+bins = np.concatenate((np.flip(np.arange(m - offset - 0.005, min_ - i, -i)),
+                       np.arange(m + offset - 0.005, max_ + i, i)))  # crea una lista di valori centrata
 
-plt.figure("Analisi dati raccolti") # da il nome al pannello
+plt.figure("Analisi dati raccolti")  # da il nome al pannello
 
-plt.hist(data,bins=bins, density=True, label= 'data', color="#89c4ff", edgecolor='black') # disegna istogramma
+plt.hist(data, bins=bins, density=True, label='data', color="#89c4ff", edgecolor='black')  # disegna istogramma
 plt.xticks(bins[:-1], rotation=45)
 
-x: Array = np.linspace(min_,max_)    # array ad alta densità
-y: Array = sc.norm.pdf(x,media,sigma) # y della distribuzione normale ad alta densità
-plt.plot(x,y,label=f"$G(x;\mu,\sigma)$", color="#ff0000",linewidth='4') # sovrappone al hist la gaussiana
+x: Array = np.linspace(min_, max_)  # array ad alta densità
+y: Array = sc.norm.pdf(x, media, sigma)  # y della distribuzione normale ad alta densità
+plt.plot(x, y, label=f"$G(x;\mu,\sigma)$", color="#ff0000", linewidth='4')  # sovrappone al hist la gaussiana
 ""
-description: str = "" + "$\\bar{t}$ = " + str(round(media,3)) + ", $\sigma$ = " + str(round(sigma,2)) + ", $\sigma^2$ = " + str(round(var,2)) + ", pvalue($\\tilde{\\chi}^2$) = " + str(CHIquadro * 100) + '%'
+description: str = "" + "$\\bar{t}$ = " + str(round(media, 3)) + ", $\sigma$ = " + str(
+    round(sigma, 2)) + ", $\sigma^2$ = " + str(round(var, 2)) + ", pvalue($\\tilde{\\chi}^2$) = " + str(
+    CHIquadro * 100) + '%'
 
 plt.ylabel("Densità di frequenza")
 plt.xlabel("$t_{caduta}$ (s)")
