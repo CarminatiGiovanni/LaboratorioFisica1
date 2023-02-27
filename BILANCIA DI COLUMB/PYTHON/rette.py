fefafc83ad6f6590acb74e0a83887e3c63fe9b2b
HeyJOe <jocarmi03@gmail.com>
2023-02-27 12:21:41 +0100

    fatta prima raccolta dati

---
+++ BILANCIA DI COLUMB/PYTHON/rette.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+FILE = dir_path + '/../CSV/' + 'misure.csv'
+
+fr = pd.read_csv(FILE)  # fileread
+distanze = np.array(fr["d(cm)"])
+teta1 = np.array(fr["teta1"])
+teta2 = np.array(fr["teta2"])
+teta3 = np.array(fr["teta3"])
+
+teta = np.array([round(np.average([teta1[i],teta2[i],teta3[i]]),0) for i in range(0,len(teta1))],dtype=np.int16)
+
+plt.plot(1/(distanze**2),teta)
+plt.show()
