f5d7b321570ce8a45ed1d31e45f4a5b058cc9479
HeyJOe <jocarmi03@gmail.com>
2023-02-28 12:29:22 +0100

    fixed delta A

--- BILANCIA DI COLUMB/PYTHON/costante_dielettrica_assoluta.py
+++ BILANCIA DI COLUMB/PYTHON/costante_dielettrica_assoluta.py
@@ -4,10 +4,10 @@ COSTANTE_DI_TORSIONE: const = 8.617290052022186e-05
 raggio: const = 0.017 # m
 
 distanza_variabile_A:const = 0.0125 #A1
-delta_distanza_variabile_A: const = 0.0047 #deltaA1
+delta_distanza_variabile_A: const = 0.0005 #deltaA1
 
 voltaggio_variabile_A: const = 0.0359 #A2
-delta_voltaggio_variabile_A: const = 0.0114 #deltaA2
+delta_voltaggio_variabile_A: const = 0.0011 #deltaA2
 
 ########################### CASO A1 ##########################
 
@@ -15,3 +15,4 @@ delta_voltaggio_variabile_A: const = 0.0114 #deltaA2
 
 
 ########################### CASO A2 ############################
+
