9f1d58186356a7acfb71560a00f539e500594733
HeyJOe <jocarmi03@gmail.com>
2023-03-01 15:49:30 +0100

    aggiunta sigmaEpsilon

--- BILANCIA DI COLUMB/PYTHON/costante_dielettrica_assoluta.py
+++ BILANCIA DI COLUMB/PYTHON/costante_dielettrica_assoluta.py
@@ -2,6 +2,7 @@ from typing import Final as const
 import numpy as np
 
 COSTANTE_DI_TORSIONE: const = 8.617290052022186e-05
+sigma_COSTANTE_DI_TORSIONE: const = 1.104980974767341e-06
 raggio: const = 0.017 # m
 
 distanza_variabile_A:const = 0.0125 #A1
@@ -13,10 +14,13 @@ delta_voltaggio_variabile_A: const = 0.11e-08 #deltaA2
 ########################### CASO A1 ##########################
 
 EPSILON = (COSTANTE_DI_TORSIONE * distanza_variabile_A) / (4*np.pi*6000*6000*np.power(raggio,2))
-print(EPSILON)
+sigmaEPSILON = EPSILON * np.sqrt(np.power(sigma_COSTANTE_DI_TORSIONE/COSTANTE_DI_TORSIONE,2)+np.power(delta_distanza_variabile_A/distanza_variabile_A,2))
+print(EPSILON, sigmaEPSILON)
 
 
 ########################### CASO A2 ############################
 
 EPSILON = (voltaggio_variabile_A * COSTANTE_DI_TORSIONE * 0.01)/(4*np.pi*np.power(raggio,2))
-print(EPSILON)
+sigmaEPSILON = EPSILON * np.sqrt(np.power(sigma_COSTANTE_DI_TORSIONE/COSTANTE_DI_TORSIONE,2)+np.power(delta_voltaggio_variabile_A/voltaggio_variabile_A,2))
+
+print(EPSILON, sigmaEPSILON)
