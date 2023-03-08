10c99d92b0fd18b069ed70250b144d85364c1d74
HeyJOe <jocarmi03@gmail.com>
2023-03-08 20:14:44 +0100

    fatto distanze_variabili

--- BILANCIA DI COLUMB/PYTHON/distanze_variabili.py
+++ BILANCIA DI COLUMB/PYTHON/distanze_variabili.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from B import calcB, calcDeltaB
+from interpolazione import RettaInterpolata
 
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-FILE = dir_path + '/../CSV/' + 'distanza_varia.csv'
+FILE = dir_path + '/../CSV/' + 'v_cost_B.csv'
 
 fr = pd.read_csv(FILE)  # fileread
 distanze = np.array(fr["d(cm)"])/ 100 #distanze in metri
@@ -23,16 +23,23 @@ tetaRad = (teta/180) * np.pi
 tetaRadConCorrezione = tetaRad / correzione_per_diametro
 
 x = 1/(distanze**2)
-B = calcB(x, tetaRadConCorrezione)
-deltaB = calcDeltaB(x, tetaRadConCorrezione)
-print(B, deltaB)
-x_B = np.linspace(min(x), max(x), 100)
-y_B = B * x_B
 
-plt.title("1/$r^2$ (1/$m^2$) - teta (rad)")
-plt.plot(x,tetaRad, color="lime", label="distanze - angoli no correzione")
-plt.plot(x, tetaRadConCorrezione, color="blue", label="distanze - angoli corretti")
-plt.plot(x_B, y_B, color="red", label="retta interpolata", linewidth="4")
-plt.xticks(x)
+r = RettaInterpolata(x, tetaRadConCorrezione, (1/180)*np.pi,'BX')
+
+plt.title("$1/d^2$ - $\\bar{\\theta}$ (rad)")
+plt.plot(r.best_x, r.best_y, color="red", label="retta interpolata", linewidth="3")
+plt.errorbar(x, tetaRadConCorrezione,yerr=r.sigmay,ecolor='black',capsize=5,fmt='o', color="blue", label="distanze - angoli corretti")
+plt.xticks(x, rotation=40)
+plt.xlabel("$1/d^2$")
+plt.ylabel("$\\bar{\\theta}$")
 plt.legend()
 plt.show()
+
+print(f"""
+    B: {r.B}
+    sigmaB: {r.sigmaB}
+    sigmaY: {r.sigmay}
+    chi: {r.chiquadro_osservato}
+    chi ridotto: {r.chiquadro_osservato_ridotto}
+    df: {r.df}
+""")
