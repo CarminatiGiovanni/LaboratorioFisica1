f5ef3153504caf89fc29de87d0c42a182e23c845
HeyJOe <jocarmi03@gmail.com>
2023-03-12 17:42:00 +0100

    per qualche ignota ragione spostando interpolazione.py e rinominandolo ha deciso di cambiare altra roba in giro, ignoro il problema e spero che in futuro continui a non avere problemi con l'interpolazione nella bilancia di columb

--- BILANCIA DI COLUMB/PYTHON/torsione_C.py
+++ BILANCIA DI COLUMB/PYTHON/torsione_C.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from interpolazione import RettaInterpolata
+from tests.interpolazione_vecchio import RettaInterpolata
 
 import os
 
