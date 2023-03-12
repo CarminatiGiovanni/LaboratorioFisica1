ad1a12e17bb26363d5d9e04f046088926baca6e6
HeyJOe <jocarmi03@gmail.com>
2023-03-12 17:36:18 +0100

    distanza variabile sistemata

--- CORDA/PYTHON/distanza_variabile.py
+++ CORDA/PYTHON/distanza_variabile.py
@@ -2,38 +2,32 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from typing import Final
-from B import calcB, calcSigmaB, calcSigmaY_y_A_BX, calcSigmaA, calcA
-# from B import calcB, calcDeltaB
+from interpolazione2 import RettaInterpolata
 
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 FILE = dir_path + '/../CSV/' + 'misurazione_distanza_variabile.csv'
 
-ERRORE_SNSIBILITA_OSCILLOSCOPIO = 1
+ERRORE_SENSIBILITA_OSCILLOSCOPIO = 1 #
 
 fr = pd.read_csv(FILE, sep = ';')  # fileread
 
-l = np.array(fr['L'])
-f1 = np.array(fr['f1'])
-f2 = np.array(fr['f2'])
+l = np.array(fr['L']) # m
+f1 = np.array(fr['f1']) # prima onda stazionaria
+f2 = np.array(fr['f2']) # seconda onda stazionaria (più precisa)
 
-# plt.plot(l,f2,'o-', label="f2")
-# plt.xticks(l)
+r1 = RettaInterpolata(1/l,f1, ERRORE_SENSIBILITA_OSCILLOSCOPIO)
+plt.plot(r1.best_x,r1.best_y, label=f"f1 $\sigma_y$={round(r1.sigmaY,2)}")
+plt.errorbar(r1.X,r1.Y, yerr=r1.sigmaY, fmt='o', ecolor='black', color="red", capsize=5)
 
-B = calcB(1/l,f2)
-A = calcA(1/l,f2)
-simgaA = calcSigmaA(1/l,f2)
-sigmaB = calcSigmaB(1/l,f2)
-sigmay = calcSigmaY_y_A_BX(1/l,f2)
-sigmay = np.sqrt(sigmay ** 2 + ERRORE_SNSIBILITA_OSCILLOSCOPIO ** 2)
+r2 = RettaInterpolata(1/l,f2, ERRORE_SENSIBILITA_OSCILLOSCOPIO)
+plt.plot(r2.best_x,r2.best_y,label=f"f2 $\sigma_y$={round(r2.sigmaY,2)}")
+plt.errorbar(r2.X,r2.Y, yerr=r2.sigmaY, fmt='o', ecolor='black', color="red", capsize=5)
 
-# plt.plot(1/l,f2,'o-', label="f2 prop inversa")
-# plt.xticks(1/l)
-x =np.linspace(min(1/l),max(1/l),100)
-y = A + B*x
-plt.plot(x,y)
-plt.errorbar(1/l,f2, yerr=sigmay, fmt='o', ecolor='black', color="red", capsize=10)
-
+plt.title("distanza variabile e frequenza per prima e seconda armonica")
+plt.xlabel('1/d ($m^(-1)$)')
+plt.ylabel('frequenza (Hz)')
+plt.xticks(np.round(1/l,2),rotation=40)
 plt.legend()
 plt.show()
