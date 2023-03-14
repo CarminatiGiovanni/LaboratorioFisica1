535bf3adee551a461624e11f267c1262e29637fe
HeyJOe <jocarmi03@gmail.com>
2023-03-14 15:07:11 +0100

    calcolati g

--- KATER/PYTHON/caduta_libera.py
+++ KATER/PYTHON/caduta_libera.py
@@ -16,35 +16,42 @@ fr = pd.read_csv(FILE)
 h = np.array(fr['h'])*0.01
 t = np.array(fr['AVG t']) * 0.001
 
-columns = list(fr.columns[1:4])
+columns = list(fr.columns[1:11])
 buffer_std = []
 for i in range(0,len(h)):
     buffer = []
     for c in columns:
         buffer.append(fr[c][i] * 0.001)
-    buffer_std.append(dev_std(np.array(buffer,dtype=np.float64))/np.sqrt(len(columns)))
+    buffer_std.append(dev_std(np.array(buffer,dtype=np.float64)))
 
-sigmaYmisurati = np.round(np.array(buffer_std,dtype=np.float64),2)
+sigmaYmisurati = np.round(np.array(buffer_std,dtype=np.float64),4)
 
 sigmaY_propagati = 2*sigmaYmisurati*t
 sigmayy = np.mean(sigmaY_propagati)
 
+# parabola tempo e h
+# plt.title("altezza(m) - tempo(s)")
+# plt.errorbar(h,t,fmt='o-',yerr=sigmaY_propagati,ecolor='black',linewidth=3,color="blue")
+# plt.xticks(np.round(h,2),rotation=40)
+# plt.show()
+
+
 r = RettaInterpolata(h,t**2, sigmayy)
-plt.plot(t,h)
-plt.plot(r.best_x,r.best_y)
-plt.errorbar(r.X,r.Y,fmt='o',yerr=r.sigmaY,color="red",ecolor="black",capsize=5)
+# plt.plot(r.best_x,r.best_y)
+# plt.errorbar(r.X,r.Y,fmt='o',yerr=r.sigmaY,color="red",ecolor="black",capsize=5)
+# plt.show()
+
 
 g = 2/r.B
 sigmag = g * (r.sigmaB / r.B)
 
-print(g,sigmag)
-plt.show()
+print('valore con t sulle y',np.round(g,3),np.round(sigmag,3))
 
 r = RettaInterpolata(t**2,h)
 g = 2*r.B
 sigmag = r.sigmaB * 2
 
-print(g,sigmag)
+print('valore con t sulle x',np.round(g,3),np.round(sigmag,3))
 
 
 
