2465d078df5da9b043b05137312bb92f51bf2496
HeyJOe <jocarmi03@gmail.com>
2023-03-20 17:12:50 +0100

    calore latente

---
+++ CALORIMETRO/PYTHON/c_h2o.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from interpolazione2 import RettaInterpolata
+
+T = np.array([21.5,22,22.5,23,23.5,24,24.5,25,26,27,28,29.5,30.5,32,34,35.5,37,39,40.5,43,45])
+t = np.arange(0,21*60,60)
+
+
+misure_buone = 10
+r = RettaInterpolata(t[:misure_buone],T[:misure_buone],0.5)
+
+plt.errorbar(r.X,r.Y,fmt='o',yerr=r.sigmaY,ecolor='black',capsize=5,color='red')
+plt.plot(r.best_x,r.best_y)
+# plt.show()
+
+print(r)
+
+
+I = 3.5 # A
+V = 15 # V
+c_h20 = 4187 
+m_h20 = 0.3 # kg
+me = 0.014 #kg
+
+J = (I*V)/(c_h20 * r.B * (m_h20 + me))
+sigmaJ = J * r.sigmaB / r.B
+print(np.round(J,2), np.round(sigmaJ,2))
+
---
+++ CALORIMETRO/PYTHON/calore_latente.py
@@ -0,0 +1,15 @@
+T_h20 = 85.5 + 273.15 # °K
+T_ice = -17 + 273.15 # °K
+T_fus = 273.15 # °k
+m_ice = 0.05 # kg
+m_h20 = 0.3 # kg
+me = 0.014
+Te = 57.5 + 273.15 # °K
+c_ice = 2051
+c_h20 = 4181
+
+calore_latente = ((m_h20 + me) * c_h20 * (T_h20 - Te) - m_ice * c_ice * (T_fus - T_ice) - m_ice * c_h20 * (Te - T_fus))/m_ice
+
+print("calore latente: ",calore_latente)
+
+
